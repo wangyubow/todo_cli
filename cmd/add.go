caafedef429801ebd290f6780992fa439df918bc
wangyubow <wangyubow@icloud.com>
2021-10-28 16:24:28 +0800

    finally

---
+++ cmd/add.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/wangyubow/todo_cli/todo"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: addRun,
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	//items := []todo.Item{}
+	items, err := todo.ReadItems(viper.GetString("dataFile"))
+
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	for _, x := range args {
+		item := todo.Item{Text: x}
+		item.SetPriority(priority)
+		items = append(items, item)
+
+	}
+
+	if err = todo.SaveItems(viper.GetString("dataFile"), items); err != nil {
+		//fmt.Errorf("%v", err)
+		fmt.Println(err.Error())
+	}
+}
+
+var priority int
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	addCmd.Flags().IntVar(&priority, "priority", 2, "Priority:1,2,3")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
